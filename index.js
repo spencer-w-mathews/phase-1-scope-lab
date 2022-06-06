88b96bc8eefa664bf4f93c9ad6d41b6cd926f29a
Spencer Mathews <smathews0906@gmail.com>
2022-06-06 16:32:35 -0500

    commit tree 77135f8996fbf655bb193fe60178e5166468803c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = 'BOB';
+}
+
+
+
+function setBestCustomer(){
+     bestCustomer= 'not bob';
+     return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+const leastFavoriteCustomer= 'bob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'bobette'
+}
